a90aa330c319907907644a6ceda1f927ed887a29
Evgeny Barannikov <e.barannikov@7rlines.com>
2022-02-15 17:19:46 +0300

    [sauc]

--- src/pages/MultilevelMenuPage/MultilevelMenuPage.tsx
+++ src/pages/MultilevelMenuPage/MultilevelMenuPage.tsx
@@ -1,8 +1,56 @@
 import './multilevelMenuPageStyles.scss';
-import SaucMLMenuFCC from '../../utils/SaucMLMenu/SaucMLMenuFCC';
+import SaucMLMenuFCC, { SaucMenuElem } from '../../utils/SaucMLMenu/SaucMLMenuFCC';
+import { ReactComponent as IconBell } from '../../utils/SaucMLMenu/icons/bell.svg';
+import { ReactComponent as IconCaret } from '../../utils/SaucMLMenu/icons/caret.svg';
 
 export default function MultilevelMenuPage() {
+  const menuElemsSTA: SaucMenuElem[] = [
+    {
+      id: 'auto',
+      body: (<div>Автомобили</div>),
+      cb: (el) => {
+        console.log('!!-!!-!! el {220215131411}\n', el) // del+
+      },
+      icon: (<IconBell/>),
+      children: [
+        {
+          id: 'su',
+          body: (<div>Отечественные</div>),
+          cb: (el) => {
+            console.log('!!-!!-!! el {220215131940}\n', el) // del+
+          }
+        },
+        {
+          id: 'in',
+          body: (<div>Иномарки</div>),
+          children: [
+            {
+              id: 'Mazda', body: (<div>Mazda</div>), cb: (el) => {
+                console.log('!!-!!-!! el {220215155830}\n', el) // del+
+              }
+            },
+            {
+              id: 'Honda', body: (<div>Honda</div>), cb: (el) => {
+                console.log('!!-!!-!! el {220215155831}\n', el) // del+
+              }
+            }
+          ],
+
+        }
+      ]
+    },
+    {
+      id: 'estate',
+      body: (<div>Недвижимость</div>),
+      cb: (el) => {
+        console.log('!!-!!-!! el {220215131412}\n', el) // del+
+      }
+    }
+  ]
+
   return (<div>
-    <SaucMLMenuFCC/>
+    <SaucMLMenuFCC menuElems={menuElemsSTA}>
+      <IconCaret className="a-icon"/>
+    </SaucMLMenuFCC>
   </div>)
 }
--- src/utils/SaucMLMenu/SaucMLMenuFCC.tsx
+++ src/utils/SaucMLMenu/SaucMLMenuFCC.tsx
@@ -1,7 +1,95 @@
 import './saucMLMenuStyles.scss';
+import { ReactComponent as IconChevron } from './icons/chevron.svg';
+import { ReactComponent as IconChevronToLeft } from './icons/chevronToLeft.svg';
+import { ReactComponent as IconCircle } from './icons/circle.svg';
+import _ from 'lodash';
+import { ReactNode, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-export default function SaucMLMenuFCC() {
-  return (<div>
-    SaucMLMenuFCC
-  </div>)
+export type SaucMenuElemId = string
+
+export interface SaucMenuElem {
+  id: SaucMenuElemId
+  icon?: JSX.Element
+  body?: JSX.Element
+  children?: SaucMenuElem[]
+  cb?: (elem: SaucMenuElem) => void
+}
+
+export interface SaucProps {
+  menuElems: SaucMenuElem[]
+  /**
+   * иконка для показа справа у пунктов меню имеющих подменю
+   */
+  iconSubmenu?: JSX.Element
+  children?: ReactNode
+}
+
+export default function SaucMLMenuFCC({menuElems, iconSubmenu = (<IconChevron/>), children}: SaucProps) {
+
+  const [$dropdownShow, $dropdownShowSet] = useState(false);
+  const [$menuElemsCurrent, $menuElemsCurrentSet] = useState<SaucMenuElem[]>(menuElems);
+  const [$backElemCurrent, $backElemCurrentSet] = useState<SaucMenuElem | null>(null);
+  const [$allBackElems, $allBackElemsSet] = useState<Array<SaucMenuElem | null>>([]);
+  const [$allMenuElems, $allMenuElemsSet] = useState<SaucMenuElem[][]>([]);
+
+  function btnMainHandle() {
+    $dropdownShowSet(!$dropdownShow)
+  }
+
+  const itemClickHandle = (elBack: SaucMenuElem, children?: SaucMenuElem[]) => () => {
+    if (children && children.length > 0) {
+      $allBackElems.push($backElemCurrent)
+      $allMenuElems.push($menuElemsCurrent)
+      debugger; // del+
+      // ---
+      $backElemCurrentSet(elBack)
+      $menuElemsCurrentSet(children)
+    }
+  };
+
+  const backHandle = () => {
+    const lastBackElem: any = $allBackElems.splice($allBackElems.length - 1, 1)
+    const lastMenuElems: any = $allMenuElems.splice($allMenuElems.length - 1, 1)
+    debugger; // del+
+    // ---
+    $backElemCurrentSet(lastBackElem[0])
+    $menuElemsCurrentSet(lastMenuElems[0])
+  }
+
+  const conf = {
+    cls: 'sauc-dropdown'
+  }
+
+  return (
+    <div className="sauc-menu">
+      <button className="sauc-menu__btn" onClick={btnMainHandle}>{children}</button>
+      <CSSTransition
+        in={$dropdownShow}
+        classNames={conf.cls}
+        timeout={200}
+      >
+        <div className={conf.cls}>
+          {!$backElemCurrent ? null : (
+            <div key={$backElemCurrent.id} className="sauc-menu__back-item" onClick={backHandle}>
+              <div className="back-item__icon"><IconChevronToLeft/></div>
+              <div className="back-item__body">{$backElemCurrent.body}</div>
+            </div>)}
+          {
+            $menuElemsCurrent.map((el: SaucMenuElem) => {
+              return (
+                <div key={el.id} className="sauc-dropdown__elem" onClick={itemClickHandle(el, el.children)}>
+                  <div className="elem__icon">{el.icon || (<IconCircle/>)}</div>
+                  <div className="elem__body">{el.body}</div>
+                  {
+                    _.isEmpty(el.children) ? null : (<div className="elem__icon-sub">{iconSubmenu}</div>)
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+      </CSSTransition>
+    </div>
+  )
 }
